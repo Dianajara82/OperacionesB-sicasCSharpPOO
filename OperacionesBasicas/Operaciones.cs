6c5a719fc4db046753e17244020c61d6cb727125
Diana Arismendi <dianajara82gmail.com>
2020-09-20 19:42:56 -0500

    Calculadora de operaciones básicas en C#, usando POO

---
+++ OperacionesBasicas/Operaciones.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OperacionesBasicas
+{
+    public class Operaciones : Principal
+    {
+        private double suma, resta, multiplo, cociente;
+
+        public void calcularSuma()
+        {
+            this.suma = this.getNumero1() + this.getNumero2();
+
+        }
+
+        public void calcularResta()
+        {
+            this.resta = this.getNumero1() - this.getNumero2();
+
+        }
+
+        public void calcularMultiplo()
+        {
+            this.multiplo = this.getNumero1() * this.getNumero2();
+
+        }
+
+        public void calcularCociente()
+        {
+            this.cociente = this.getNumero1() / this.getNumero2();
+
+        }
+
+        public double getSuma()
+        {
+            return this.suma;
+        }
+
+        public double getResta()
+        {
+            return this.resta;
+        }
+
+        public double getMultiplo()
+        {
+            return this.multiplo;
+        }
+
+        public double getCociente()
+        {
+            return this.cociente;
+        }
+    }
+}
